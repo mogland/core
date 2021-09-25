0043c60e882e496e7569686b781837083fac7ac1
wibus-wee <1596355173@qq.com>
2021-09-25 08:29:06 +0800

    perf: return host/admin

--- src/host/host.controller.ts
+++ src/host/host.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpCode } from '@nestjs/common';
 
 @Controller('host')
-export class HostController {}
+export class HostController {
+
+    @Get('admin')
+    @HttpCode(200)
+    async find(){
+        // connect to database (TODO)
+        let data = {}
+        Object.defineProperty(data, "name", {
+            value: "Wibus",
+            writable: false,
+        })
+        Object.defineProperty(data, "description", {
+            value: "Just Uaeua",
+            writable: false,
+        })
+        Object.defineProperty(data, "image", {
+            value: "http://q1.qlogo.cn/g?b=qq&nk=1596355173&s=640",
+            writable: false,
+        })
+        Object.defineProperty(data, "github", {
+            value: "wibus-wee",
+            writable: false,
+        })
+        return data;
+    }
+}
