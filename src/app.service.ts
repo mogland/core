549205b5a9eb5731d9bc028596692c144ed2c81d
wibus-wee <1596355173@qq.com>
2021-09-30 23:40:10 +0800

    chore: add mogoose db tool

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,14 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+  connect(){
+    var mongoose = require('mongoose');
+    // mongoose.connect('mongodb://localhost/test');
+    var db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'connection error:'));
+    db.once('open', function() {
+    console.log('we\'re connected')
+    return 'we\'re connected'
+    });
+  }
 }
