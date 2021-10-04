ca93a387be2f4ae315cb2ca3628c9b3b6ccfbae2
wibus-wee <1596355173@qq.com>
2021-10-04 23:40:23 +0800

    perf(swagger): optimize the swagger of comment

--- comment/comment.controller.ts
+++ comment/comment.controller.ts
@@ -1,8 +1,11 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { CommentService } from './comment.service';
 import { CreateCommentDto } from './create-comment-dto';
 
 @Controller('comment')
+@ApiTags("Comment")
 export class CommentController {
 
     constructor(
@@ -10,16 +13,27 @@ export class CommentController {
     ){}
 
     @Get(":type/:path")
+    @ApiOperation({
+        summary: "获取文章/页面中的评论"
+    })
     async get(@Param() param){
         return await this.commentService.getComment(param.type, param.path)
     }
 
     @Post("create")
+    @ApiOperation({
+        summary: "发布评论"
+    })
+    @UseGuards(AuthGuard('jwt'))
     async create(@Body() data: CreateCommentDto[]){
         return await this.commentService.createComment(data)
     }
 
     @Post("delete")
+    @ApiOperation({
+        summary: "删除评论"
+    })
+    @UseGuards(AuthGuard('jwt'))
     async delete(@Body() data){
         return await this.commentService.deleteComment(data.cid)
     }
--- comment/create-comment-dto.ts
+++ comment/create-comment-dto.ts
@@ -1,26 +1,56 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 /*
  * @FilePath: /nest-server/comment/create-comment-dto.ts
  * @author: Wibus
  * @Date: 2021-10-04 22:04:15
  * @LastEditors: Wibus
- * @LastEditTime: 2021-10-04 23:25:41
+ * @LastEditTime: 2021-10-04 23:38:15
  * Coding With IU
  */
 export class CreateCommentDto {
     cid: number; //comment id
+
+    @ApiProperty()
     type: string //choose `post` or `page`
+
+    @ApiProperty()
     path: string
+
     post: string //only ID
+
+    @ApiProperty()
     content: string //comment content
+
     createTime: number
+
+    @ApiProperty()
     author: string
+
+    @ApiProperty()
     owner: String
+
+    @ApiProperty()
     isOwner?: Boolean = true
+
+    @ApiProperty()
     email: string
+
+    @ApiProperty()
     url?: string = null
+
+    @ApiProperty()
     key?: String = null
+
+    @ApiProperty()
     hasChild: Boolean = false
+
+    @ApiProperty()
     ipAddress?: string = null
+
+    @ApiProperty()
     userAgent?: string = null
+    
+    @ApiProperty()
     state: number = 0 // 0 need checked, 1 push, 2 shit message
 }
