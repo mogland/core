a390028d4ba09e77adad9ba54b99603c3f6c09c8
wibus-wee <1596355173@qq.com>
2021-10-05 15:59:32 +0800

    perf(configs): set `jwtToken` for auth constants

--- configs.ts
+++ configs.ts
@@ -3,11 +3,11 @@
  * @author: Wibus
  * @Date: 2021-10-04 15:21:24
  * @LastEditors: Wibus
- * @LastEditTime: 2021-10-04 15:45:24
+ * @LastEditTime: 2021-10-05 15:57:37
  * Coding With IU
  */
 let configs = {
-    expiration: 3600 //登陆过期时间(s)
-    // 一天有86400秒，一个月有2592000秒
+    expiration: 86400, //登陆过期时间(s)
+    jwtToken: "bU2-sD4_fT2-qK3-dO2&hN5+iY2!mV7+" //登陆Token
 }
 export default configs
