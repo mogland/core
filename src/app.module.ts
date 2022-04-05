bac6b08a598d3fd2c0d790c41ecbf4ba0b834120
wibus-wee <1596355173@qq.com>
2022-04-05 17:59:17 +0800

    ci(test): test after building

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,19 @@
 import { Module } from "@nestjs/common"; // 引入模块
-import { ConfigModule, ConfigService } from '@nestjs/config'; // 引入配置文件
+import { ConfigModule } from '@nestjs/config'; // 引入配置文件
 import { APP_FILTER, APP_GUARD } from "@nestjs/core"; // 引入守卫
 import { JwtModule } from "@nestjs/jwt"; // 引入jwt模块
 import { PassportModule } from "@nestjs/passport"; // 引入passport模块
+import { ScheduleModule } from '@nestjs/schedule';
 import { TypeOrmModule } from "@nestjs/typeorm"; // 引入typeorm模块
-import { AllExceptionFilter } from "./common/filters/all-exception.filter"; // 引入过滤器
-import { RolesGuard } from "./common/guards/roles.guard"; // 引入守卫
-import { FriendsModule } from "./modules/friends/friends.module"; // 引入好友模块
-import { MailController } from "./modules/mail/mail.controller"; // 引入邮件模块
-import { MailService } from "./modules/mail/mail.service"; // 引入邮件模块
-import { UsersController } from "./modules/users/users.controller"; // 引入用户模块
+import { GHttp } from "helper/helper.http.service";
+import { ProjectsController } from "modules/projects/projects.controller"; // 引入项目模块
+import { ProjectsModule } from "modules/projects/projects.module"; // 引入项目模块
+import { ProjectsService } from "modules/projects/projects.service"; // 引入项目模块
+import { chooseEnv } from "utils/chooseEnv.utils";
 import { AppController } from "./app.controller"; // 引入控制器
 import { AppService } from "./app.service"; // 引入服务
+import { AllExceptionFilter } from "./common/filters/all-exception.filter"; // 引入过滤器
+import { RolesGuard } from "./common/guards/roles.guard"; // 引入守卫
 import configs from "./configs"; // 引入配置文件
 import { AuthModule } from "./modules/auth/auth.module"; // 引入认证模块
 import { AuthService } from "./modules/auth/auth.service"; // 引入认证模块
@@ -25,23 +27,20 @@ import { CommentsController } from "./modules/comments/comments.controller"; // 
 import { CommentsModule } from "./modules/comments/comments.module"; // 引入评论模块
 import { CommentsService } from "./modules/comments/comments.service"; // 引入评论模块
 import { FriendsController } from "./modules/friends/friends.controller"; // 引入好友模块
+import { FriendsModule } from "./modules/friends/friends.module"; // 引入好友模块
 import { FriendsService } from "./modules/friends/friends.service"; // 引入好友模块
+import { MailController } from "./modules/mail/mail.controller"; // 引入邮件模块
 import { MailModule } from "./modules/mail/mail.module"; // 引入邮件模块
+import { MailService } from "./modules/mail/mail.service"; // 引入邮件模块
 import { PagesController } from "./modules/pages/pages.controller"; // 引入页面模块
 import { PagesModule } from "./modules/pages/pages.module"; // 引入页面模块
 import { PagesService } from "./modules/pages/pages.service"; // 引入页面模块
 import { PostsController } from "./modules/posts/posts.controller"; // 引入文章模块
 import { PostsModule } from "./modules/posts/posts.module"; // 引入文章模块
 import { PostsService } from "./modules/posts/posts.service"; // 引入文章模块
+import { UsersController } from "./modules/users/users.controller"; // 引入用户模块
 import { UsersModule } from "./modules/users/users.module"; // 引入用户模块
 import { UsersService } from "./modules/users/users.service"; // 引入用户模块
-import configuration from "./utils/getDataConfig.util"; // 引入配置文件
-import { ProjectsModule } from "modules/projects/projects.module"; // 引入项目模块
-import { ProjectsController } from "modules/projects/projects.controller"; // 引入项目模块
-import { ProjectsService } from "modules/projects/projects.service"; // 引入项目模块
-import { GHttp } from "helper/helper.http.service";
-import { ScheduleModule } from '@nestjs/schedule'
-import { chooseEnv } from "utils/chooseEnv.utils";
 
 
 @Module({
--- src/utils/chooseEnv.utils.ts
+++ src/utils/chooseEnv.utils.ts
@@ -5,9 +5,12 @@ import { argv } from "zx";
  * @author: Wibus
  * @Date: 2022-03-20 16:23:37
  * @LastEditors: Wibus
- * @LastEditTime: 2022-03-20 21:37:15
+ * @LastEditTime: 2022-04-05 17:58:42
  * Coding With IU
  */
 export const chooseEnv = (name: any) => {
+  // return argv.hasOwnProperty(name) ? argv[name] : process.env[name];
+  // console.log(argv[name])
   return argv.hasOwnProperty(name) ? argv[name] : process.env[name];
+  
 }
