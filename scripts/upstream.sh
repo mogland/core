de4088fef0dda65be31780b41951c488968de3f3
wibus-wee <1596355173@qq.com>
2022-04-23 06:49:57 +0800

    ci(upstream): add safe.directory

--- scripts/upstream.sh
+++ scripts/upstream.sh
@@ -3,17 +3,19 @@
  # @author: Wibus
  # @Date: 2022-03-06 20:22:17
  # @LastEditors: Wibus
- # @LastEditTime: 2022-03-07 22:54:08
+ # @LastEditTime: 2022-04-23 06:47:41
  # Coding With IU
 ### 
 # git remote -v
 # 如果 git remote -v 命令 不包含wibus-wee
 if [ ! -n "$(git remote -v | grep wibus-wee)" ]; then
+git init
 git remote add upstream https://github.com/wibus-wee/GS-server.git
 git remote -v
 git fetch upstream
 git checkout main
 git merge upstream/main
+git config --global --add safe.directory "*"
 git push origin main
 else
 echo "非组织远程仓库，无需操作"
