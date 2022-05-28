9a519e5a27b637416a7dc051b24ac06574975c75
Wibus <1596355173@qq.com>
2022-05-28 16:28:08 +0800

    ci(upstream): change synchronization remote

--- scripts/upstream.sh
+++ scripts/upstream.sh
@@ -1,24 +1,22 @@
 ###
- # @FilePath: /GS-server/scripts/upstream.sh
+ # @FilePath: /nx-server/scripts/upstream.sh
  # @author: Wibus
  # @Date: 2022-03-06 20:22:17
  # @LastEditors: Wibus
- # @LastEditTime: 2022-05-01 00:30:18
+ # @LastEditTime: 2022-05-28 16:27:15
  # Coding With IU
 ### 
 # git remote -v
-# 如果 git remote -v 命令 不包含wibus-wee
-git config --global --add safe.directory /github/workspace
-if [ ! -n "$(git remote -v | grep wibus-wee)" ]; then
+# 如果 git remote -v 命令 不包含nx-space
+if [ ! -n "$(git remote -v | grep nx-space)" ]; then
 git init
-git remote add upstream https://github.com/wibus-wee/GS-server.git
+git remote add upstream https://github.com/nx-space/nx-server.git
 git remote -v
 git fetch upstream
 git checkout main
 git merge upstream/main
-git config --global --add safe.directory /github/workspace
 git push origin main
 else
-echo "非组织远程仓库，无需操作"
+echo "非个人远程仓库，无需操作"
 exit
 fi
