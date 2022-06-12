3b5403a1fe890d3f5e0d71f7a91e83e13d63db8b
wibus-wee <1596355173@qq.com>
2022-06-12 18:06:10 +0800

    chore(db): change provider name

--- libs/db/src/db.module.ts
+++ libs/db/src/db.module.ts
@@ -2,7 +2,7 @@ import { Global, Module, Provider } from '@nestjs/common';
 import { CategoryModel } from '~/modules/category/category.model';
 import { PostModel } from '~/modules/post/post.model';
 import { UserModel } from '~/modules/user/user.model';
-import { databaseProvider } from './database.provider';
+import { databaseProvider } from './db.provider
 import { DbService } from './db.service';
 import { getProviderByTypegooseClass } from './model.transformer';
 
