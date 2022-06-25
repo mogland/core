4b26030180762db7d9df35b2997583352c7db676
wibus-wee <1596355173@qq.com>
2022-06-25 20:39:36 +0800

    feat(service): init configs

--- src/modules/configs/configs.service.ts
+++ src/modules/configs/configs.service.ts
@@ -4,6 +4,7 @@ import { ReturnModelType } from '@typegoose/typegoose';
 import { CacheService } from '~/processors/cache/cache.service';
 import { UserService } from '../user/user.service';
 import { generateInitConfigs } from './configs.init';
+import { ConfigsInterface } from './configs.interface';
 import { ConfigsModel } from './configs.model';
 
 @Injectable()
@@ -16,10 +17,33 @@ export class ConfigsService {
     private readonly redis: CacheService
   ) {
     this.logger = new Logger(ConfigsService.name)
+    this.initConfigs().then(() => {
+      this.logger.log('ConfigsService 初始化完成')
+    })
   }
   private configInited = false // 是否已经初始化过
 
   public get defaultConfig() {
     return generateInitConfigs()
+  }
+
+  allOptionKeys: Set<keyof ConfigsInterface> = new Set() // 全部配置项
+
+  /**
+   * initConfigs 初始化配置
+   */
+  protected async initConfigs() {
+    const configs = await this.configModel.find().lean()
+    const mergedConfigs = this.defaultConfig
+    configs.forEach(config => { // 合并配置
+      const name = config.name as keyof ConfigsInterface // let's make sure the name is correct
+      if (!this.allOptionKeys.has(name)) { // 如果这个配置项不存在
+        this.allOptionKeys.add(name) // 添加到全部配置项中
+      }
+      mergedConfigs[name] = {
+        ...mergedConfigs[name],
+        ...config.value
+      }
+    })
   }
 }
