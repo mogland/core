e56c4cf7f7ace78a0f6c4f41eac3c1b6bd08d914
Wibus <1596355173@qq.com>
2022-06-28 19:28:13 +0800

    feat: patch configs re #205

--- src/modules/configs/configs.service.ts
+++ src/modules/configs/configs.service.ts
@@ -1,6 +1,7 @@
 import { InjectModel } from '@app/db/model.transformer';
 import { Injectable, Logger } from '@nestjs/common';
-import { ReturnModelType } from '@typegoose/typegoose';
+import { ReturnModelType, DocumentType } from '@typegoose/typegoose';
+import { cloneDeep, mergeWith } from 'lodash';
 import { RedisKeys } from '~/constants/cache.constant';
 import { CacheService } from '~/processors/cache/cache.service';
 import { getRedisKey } from '~/utils/redis.util';
@@ -8,7 +9,9 @@ import { UserService } from '../user/user.service';
 import { generateInitConfigs } from './configs.init';
 import { ConfigsInterface } from './configs.interface';
 import { ConfigsModel } from './configs.model';
-
+import { LeanDocument } from 'mongoose'
+import { UserModel } from '../user/user.model';
+import { BeAnObject } from '@typegoose/typegoose/lib/types';
 @Injectable()
 export class ConfigsService {
   private logger: Logger
@@ -90,5 +93,46 @@ export class ConfigsService {
     } while (currentCount < maxCount)
 
     throw `${currentCount} 次重试 ConfigsService 初始化失败`
+  }
+
+  /**
+   * patch 更新配置
+   * @param key 配置项名称
+   * @param data 配置项数据
+   * @returns {Promise<ConfigsInterface[T]>}
+   */
+  public async patch<T extends keyof ConfigsInterface>(
+    key: T,  // 配置项名称
+    data: Partial<ConfigsInterface[T]> // 配置项数据
+    ): Promise<ConfigsInterface[T]> {
+    const config = await this.getConfig() // 获取配置
+    const updatedConfigRow = await this.configModel // 获取配置模型
+      .findOneAndUpdate( // 更新配置
+        { name: key as string }, // 查询条件, 只更新 name 为 key 的配置
+        {
+          value: mergeWith(cloneDeep(config[key]), data, (old, newer) => { // 合并配置。如果有新的配置，就覆盖旧的配置
+            // 数组不合并
+            if (Array.isArray(old)) {
+              return newer
+            }
+            // 对象合并
+            if (typeof old === 'object' && typeof newer === 'object') {
+              return { ...old, ...newer } 
+            }
+          }),
+        },
+        { upsert: true, new: true },  // 如果不存在，就创建一个新的配置
+      )
+      .lean() // 获取配置，并且转换为对象
+    const newData = updatedConfigRow.value // 获取更新后的配置
+    const mergedFullConfig = Object.assign({}, config, { [key]: newData }) // 合并配置，把更新后的配置添加到配置中
+    await this.setConfig(mergedFullConfig) // 将配置写入 redis
+    return newData
+  }
+
+  get getUser() {
+    return this.userService.getMaster.bind(this.userService) as () => Promise<
+      LeanDocument<DocumentType<UserModel, BeAnObject>>
+    >
   }
 }
