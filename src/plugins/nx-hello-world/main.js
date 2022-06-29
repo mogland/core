4443dac2ca868166a42a3becdd73c5918631269c
Wibus <1596355173@qq.com>
2022-06-29 22:52:43 +0800

    perf: add fields for mainifest

---
+++ src/plugins/nx-hello-world/main.js
@@ -0,0 +1,21 @@
+/*
+ * @FilePath: /nx-core/src/plugins/nx-hello-world/main.js
+ * @author: Wibus
+ * @Date: 2022-06-29 22:27:16
+ * @LastEditors: Wibus
+ * @LastEditTime: 2022-06-29 22:50:34
+ * Coding With IU
+ */
+
+/**
+ * main entry of the plugin
+ * @param {any} data 数据
+ * @param {*} model 模型
+ * @returns 
+ */
+export function main(data, model){
+    return {
+        data,
+        model
+    };
+}
