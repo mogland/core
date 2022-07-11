5fdc0c0f23bc57b3e835a1e04af372f30ef316b9
wibus-wee <1596355173@qq.com>
2022-07-11 21:27:53 +0800

    revert(utils): remove original rss parser

---
+++ src/utils/rss-parser.utils.ts
@@ -0,0 +1,8 @@
+/*
+ * @FilePath: /nx-core/src/utils/rss-parser.utils.ts
+ * @author: Wibus
+ * @Date: 2022-07-11 21:25:12
+ * @LastEditors: Wibus
+ * @LastEditTime: 2022-07-11 21:25:13
+ * Coding With IU
+ */
