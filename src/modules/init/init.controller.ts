b0799bb04c930db53a32f1fc8abc000777f3385f
wibus-wee <1596355173@qq.com>
2022-07-13 23:55:53 +0800

    perf(init): cancel custom config init to default

--- src/modules/init/init.controller.ts
+++ src/modules/init/init.controller.ts
@@ -1,6 +1,7 @@
-import { BadRequestException, Body, Controller, ForbiddenException, Get, Param, Patch, UnprocessableEntityException } from '@nestjs/common';
+import { BadRequestException, Body, Controller, ForbiddenException, Get, Logger, Param, Patch, UnprocessableEntityException } from '@nestjs/common';
 import { ApiOperation } from '@nestjs/swagger';
 import { ApiName } from '~/common/decorator/openapi.decorator';
+import { ConfigsInterface } from '../configs/configs.interface';
 import { ConfigsService } from '../configs/configs.service';
 import { InitKeyDto } from './init.dto';
 import { InitService } from './init.service';
@@ -15,29 +16,39 @@ export class InitController {
 
   @Get("/")
   @ApiOperation({ summary: '获取初始化情况' })
-  async isInit() {
+  async canInit() {
     return {
-      isInit: await this.init.isInit(),
+      canInit: await this.init.canInit(),
     }
   }
 
-  @Get("/configs/default")
+  @Get("/configs/get/default")
   @ApiOperation({ summary: '获取默认配置' })
   async initDefault() {
-    const { isInit } = await this.isInit();
-    if (isInit) throw new ForbiddenException("默认设置在完成注册之后不可见");
+    const { canInit } = await this.canInit();
+    if (!canInit) throw new ForbiddenException("无法获取默认配置，请检查控制台日志");
     return this.configs.defaultConfig;
 
   }
 
-  @Patch("/configs/:key")
-  @ApiOperation({ summary: '初始化与更新配置' })
-  async setConfig(@Param() params: InitKeyDto, @Body() body: Record<string, any>) {
-    const { isInit } = await this.isInit();
-    if (isInit) throw new BadRequestException("已经完成初始化, 请登录后进行设置");
-    if (typeof body !== 'object') {
-      throw new UnprocessableEntityException('body must be object')
+  @Get("/configs/default")
+  @ApiOperation({ summary: '初始化默认配置' })
+  async setConfig() {
+    const { canInit } = await this.canInit();
+    if (!canInit) throw new BadRequestException("无法完成初始化，请检查控制台日志");
+    const defaultConfig = this.configs.defaultConfig;
+    for (const theKey in defaultConfig) {
+      Logger.warn(`正在初始化默认配置: ${theKey}`);
+      const key = theKey as keyof ConfigsInterface; // convert to <keyof ConfigsInterface>
+      if (defaultConfig[key] !== undefined) {
+        const element = defaultConfig[key];
+        await this.configs.patchAndValidate(key, element);
+      }
     }
-    return this.configs.patchAndValidate(params.key, body)
+    Logger.warn(`初始化默认配置完成`);
+    return {
+      canInit: false,
+      inited: true,
+    }
   }
 }
