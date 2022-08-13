00be7c8e7b030f33110c2622eff32e8b002d491b
wibus-wee <1596355173@qq.com>
2022-08-13 23:43:59 +0800

    perf(theme): add detail pages interface

--- src/modules/theme/theme.interface.ts
+++ src/modules/theme/theme.interface.ts
@@ -3,10 +3,11 @@
  * @author: Wibus
  * @Date: 2022-08-13 23:14:55
  * @LastEditors: Wibus
- * @LastEditTime: 2022-08-13 23:33:06
+ * @LastEditTime: 2022-08-13 23:42:37
  * Coding With IU
  */
 
+import { Aggregate } from "mongoose";
 import { CategoryModel } from "../category/category.model";
 import { SiteDto, ThemeDto, UrlsDto } from "../configs/configs.dto";
 import { PageModel } from "../page/page.model";
@@ -28,7 +29,22 @@ interface ConfigsThemeInterface {
 export interface ThemeBasicInterface {
   site: SiteThemeInterface, // 网站变量
   configs: ConfigsThemeInterface, // 网站配置
-  page: PostModel | PageModel, // 针对该页面的内容
   path: string, // 页面路径
   url: string, // 页面url
+}
+
+export interface IndexThemeInterface extends ThemeBasicInterface {
+  aggregate: Aggregate<PostModel | PageModel>, // 当前页面的文章列表
+}
+
+export interface PostThemeInterface extends ThemeBasicInterface {
+  page: PostModel, // 针对该页面的内容
+}
+
+export interface PageThemeInterface extends ThemeBasicInterface {
+  page: PageModel, // 针对该页面的内容
+}
+
+export interface CategoryThemeInterface extends ThemeBasicInterface {
+  page: CategoryModel, // 针对该页面的内容
 }
