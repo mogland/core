3464e61e3891d1b9fdd066e7e3002acc75817505
wibus-wee <1596355173@qq.com>
2022-08-13 22:54:14 +0800

    perf(theme): use `Auth()` decorator for admin actions

--- src/modules/theme/theme.controller.ts
+++ src/modules/theme/theme.controller.ts
@@ -1,30 +1,43 @@
-import { Controller, Get, Query } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { Controller, Get, Param, Query, Res } from '@nestjs/common';
+import { fileTypeFromBuffer, fileTypeFromFile, fileTypeStream } from 'file-type';
+import { join } from 'path';
+import { Auth } from '~/common/decorator/auth.decorator';
 import { ApiName } from '~/common/decorator/openapi.decorator';
+import { THEME_DIR } from '~/constants/path.constant';
 import { ThemeService } from './theme.service';
+import mime from 'mime';
 
 @Controller('theme')
 @ApiName
 export class ThemeController {
   constructor(
     private readonly themeService: ThemeService,
-  ) {}
+  ) { }
+
+  // ********************************************************
+  // 以下是管理命令
 
   @Get('/admin/available')
+  @Auth()
   async availableThemes() {
     return this.themeService.availableThemes();
   }
 
   @Get('/admin/up')
+  @Auth()
   async turnOnTheme(@Query('name') name: string) {
     return this.themeService.turnOnTheme(name);
   }
 
   @Get('/admin/current')
+  @Auth()
   async currentTheme() {
     return this.themeService.currentTheme();
   }
 
   @Get('/admin/off')
+  @Auth()
   async turnOffTheme(@Query('name') name: string) {
     return this.themeService.turnOffTheme(name);
   }
