3cc11f328616e6f6932b263376874c4830405fba
wibus-wee <1596355173@qq.com>
2022-08-13 22:53:17 +0800

    feat(theme): static files provider

--- src/modules/theme/theme.controller.ts
+++ src/modules/theme/theme.controller.ts
@@ -28,4 +28,19 @@ export class ThemeController {
   async turnOffTheme(@Query('name') name: string) {
     return this.themeService.turnOffTheme(name);
   }
+
+  // ********************************************************
+  // 以下是针对静态资源访问的接口
+  @Get('/public/*')
+  async public(@Res() res, @Param() param: string) {
+    const filePath = join(THEME_DIR, (await this.themeService.currentTheme())!.name, "public", param['*']);
+    const file = await fs.readFile(filePath);
+    if (!file) {
+      return res.status(404).send();
+    } else {
+      res.type(mime.getType(filePath));
+      res.send(file);
+    }
+  }
+
 }
