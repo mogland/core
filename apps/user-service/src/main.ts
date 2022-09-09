9abf90c510b7af482f398323ae0bf280c4cf0082
Wibus <1596355173@qq.com>
2022-09-09 21:48:44 +0800

    perf: register global module

--- apps/user-service/src/main.ts
+++ apps/user-service/src/main.ts
@@ -1,15 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { registerStdLogger } from '~/apps/core/src/global/consola.global';
+import { registerGlobal } from '~/apps/core/src/global/index.global';
 import { UserServiceModule } from './user-service.module';
 
 async function bootstrap() {
+  registerGlobal();
+  registerStdLogger();
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     UserServiceModule,
     {
       transport: Transport.TCP,
     },
-  )
+  );
   app.listen();
-  
 }
 bootstrap();
