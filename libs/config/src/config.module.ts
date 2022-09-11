be7ea91311288196b8233034f769c9c83778259f
wibus-wee <1596355173@qq.com>
2022-09-11 08:07:45 +0800

    perf: get user info from token

--- libs/config/src/config.module.ts
+++ libs/config/src/config.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { UserModule } from '~/apps/core/src/modules/user/user.module';
 import { DatabaseModule } from '~/libs/database/src';
 import { ConfigService } from './config.service';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, UserModule],
   providers: [ConfigService],
   exports: [ConfigService],
 })
