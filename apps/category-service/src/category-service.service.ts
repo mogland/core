ff5de5ea840a9a0ba157083a723861ff5119b3d2
wibus-wee <1596355173@qq.com>
2022-09-18 11:20:49 +0800

    feat(service): find category info by id

--- apps/category-service/src/category-service.service.ts
+++ apps/category-service/src/category-service.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { ReturnModelType } from '@typegoose/typegoose';
+import { InjectModel } from '~/libs/database/src/model.transformer';
+import { CategoryModel } from './category.model';
 
 @Injectable()
 export class CategoryService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    @InjectModel(CategoryModel)
+    private readonly categoryModel: ReturnModelType<typeof CategoryModel>,
+  ) {}
+
+  get model() {
+    return this.categoryModel;
+  }
+
+  /**
+   * 通过 categoryID 获取分类
+   * @param id 分类 ID
+   */
+  async findCategoryById(id: string) {
+    return this.categoryModel.findById(id).lean();
   }
 }
