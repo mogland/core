d248b89cc57780121f0a97546e5f0bd0868382f6
wibus-wee <1596355173@qq.com>
2022-10-01 20:54:36 +0800

    feat: aggregate controller in module

--- apps/core/src/modules/aggregate/aggregate.module.ts
+++ apps/core/src/modules/aggregate/aggregate.module.ts
@@ -3,17 +3,18 @@
  * @author: Wibus
  * @Date: 2022-10-01 19:50:35
  * @LastEditors: Wibus
- * @LastEditTime: 2022-10-01 19:54:55
+ * @LastEditTime: 2022-10-01 20:54:25
  * Coding With IU
  */
 
 import { Module } from '@nestjs/common';
 import { PageServiceModule } from '~/apps/page-service/src/page-service.module';
+import { AggregateController } from './aggregate.controller';
 import { AggregateService } from './aggregate.service';
 
 @Module({
   imports: [PageServiceModule],
-  controllers: [],
+  controllers: [AggregateController],
   providers: [AggregateService],
   exports: [AggregateService],
 })
