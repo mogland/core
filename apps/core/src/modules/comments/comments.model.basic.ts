3287d62e840dd1f68bfbb5d3783eedb530c8aed0
Wibus <1596355173@qq.com>
2022-10-03 17:20:22 +0800

    feat: basic comment model

---
+++ apps/core/src/modules/comments/comments.model.basic.ts
@@ -0,0 +1,112 @@
+/*
+ * @FilePath: /mog-core/apps/core/src/modules/comments/comments.model.basic.ts
+ * @author: Wibus
+ * @Date: 2022-10-03 16:56:56
+ * @LastEditors: Wibus
+ * @LastEditTime: 2022-10-03 17:19:18
+ * Coding With IU
+ */
+
+import { ApiProperty } from '@nestjs/swagger';
+import { modelOptions, prop, Ref } from '@typegoose/typegoose';
+import { IsOptional, IsString } from 'class-validator';
+import { Types } from 'mongoose';
+import { PageModel } from '~/apps/page-service/src/model/page.model';
+import { PostModel } from '~/apps/page-service/src/model/post.model';
+import { BaseModel } from '~/shared/model/base.model';
+
+enum CommentStatus {
+  Pending = 'pending', // 待审核
+  Approved = 'approved', // 已通过
+  Spam = 'spam', // 垃圾评论
+  Trash = 'trash', // 回收站
+}
+
+export enum CommentType {
+  Post = 'post',
+  Page = 'page',
+}
+
+@modelOptions({ options: { customName: 'Comment' } })
+export class CommentsBasicModel extends BaseModel {
+  @prop({ required: true })
+  @IsString()
+  @ApiProperty({ description: '评论内容' })
+  text!: string;
+
+  @prop({ required: true })
+  @IsString()
+  @ApiProperty({ description: '评论者' })
+  author: string;
+
+  @prop({ required: true })
+  @IsString()
+  @ApiProperty({ description: '评论者邮箱' })
+  email: string;
+
+  @prop({ required: false })
+  @IsOptional()
+  @IsString()
+  @ApiProperty({ description: '评论者网址' })
+  url?: string;
+
+  @prop({ select: false })
+  @IsString()
+  @IsOptional()
+  @ApiProperty({ description: '评论者IP' })
+  ip?: string;
+
+  @prop()
+  @ApiProperty({ description: '评论地点, 根据IP获取归属地' })
+  @IsString()
+  @IsOptional()
+  public location?: string;
+
+  @prop({ select: false })
+  @IsString()
+  @IsOptional()
+  @ApiProperty({ description: '评论者UA' })
+  agent?: string;
+
+  @prop({ required: true, enum: CommentStatus })
+  @ApiProperty({ description: '评论状态' })
+  @IsString()
+  status: CommentStatus;
+
+  @prop({ required: true })
+  @IsString()
+  @ApiProperty({ description: '评论所属文章或页面' })
+  parent: string;
+
+  @prop({ required: true, enum: CommentType, default: CommentType.Post })
+  @ApiProperty({ description: '评论类型' })
+  type: CommentType;
+
+  @prop({ ref: () => CommentsBasicModel, type: Types.ObjectId })
+  children?: Ref<CommentsBasicModel>[]; // 子评论
+
+  @prop({ default: 1 })
+  commentsIndex?: number; // 评论顺序
+
+  @prop()
+  key?: string; // 评论key
+
+  @prop({ refPath: 'type' })
+  ref: Ref<PostModel | PageModel>; // 引用的文章或页面
+
+  @prop({
+    ref: () => PostModel, // 评论的文章
+    foreignField: '_id', // 指定外键字段
+    localField: 'ref', // 指定当前字段
+    justOne: true, // 只查询一条
+  })
+  public post: Ref<PostModel>;
+
+  @prop({
+    ref: () => PageModel,
+    foreignField: '_id',
+    localField: 'ref',
+    justOne: true,
+  })
+  public page: Ref<PageModel>;
+}
