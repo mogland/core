b444f489587f445b5eb223491de6f653c5ee5586
wibus-wee <1596355173@qq.com>
2022-11-17 14:25:04 +0800

    perf: use commander argv

--- shared/utils/read-env.ts
+++ shared/utils/read-env.ts
@@ -3,7 +3,7 @@
  * @author: Wibus
  * @Date: 2022-11-15 22:57:49
  * @LastEditors: Wibus
- * @LastEditTime: 2022-11-17 13:33:33
+ * @LastEditTime: 2022-11-17 14:23:31
  * Coding With IU
  */
 
@@ -11,8 +11,9 @@ import { join } from 'path';
 import { cwd } from 'shared/global/env.global';
 import { argv as zxArev } from 'zx-cjs';
 import yaml from 'js-yaml';
-import { readFile } from 'fs/promises';
 import { camelToUnderline } from './name';
+import { ServicesEnum } from '../constants/services.constant';
+import { readFileSync } from 'fs';
 
 // node dist/apps/core/main.js \
 // 	--userService_host=192.168.101.2 \
@@ -25,26 +26,28 @@ import { camelToUnderline } from './name';
 // node dist/apps/core/main.js \
 //   --config=core.yml
 
-export const readEnv = async (argv: typeof zxArev, path?: string) => {
+export const readEnv: (
+  service: ServicesEnum,
+  argv: typeof zxArev,
+  path?: string,
+) => {
+  [key: string]: any;
+} = (service, argv, path) => {
   const config = {};
   delete argv._; // 删除无用的参数
   delete argv.config; // 删除 config 参数，这是用来指定配置文件的
   if (!(Object.keys(argv).length === 0 && argv.constructor === Object)) {
     for (const key in argv) {
-      const [service, ...keyName] = key.split('_');
-      if (service && keyName) {
-        if (!config[camelToUnderline(service)])
-          config[camelToUnderline(service)] = {};
-        config[camelToUnderline(service)][keyName.join('_')] = argv[key];
-      }
+      if (!config[service.toLocaleLowerCase()])
+        config[service.toLocaleLowerCase()] = {};
+      config[service.toLocaleLowerCase()][key] = argv[key];
     }
   } else {
     const envPath = path || join(cwd, '.env.yaml');
-    const env = (await readFile(envPath, 'utf-8').then((data) => {
-      if (!data) return {};
-      return yaml.load(data);
-    })) as Record<string, any>;
-    Object.assign(config, camelToUnderline(env));
+    const env = readFileSync(envPath, 'utf-8');
+    if (!env) return config;
+    const envObj = yaml.load(env);
+    Object.assign(config, camelToUnderline(envObj));
   }
   console.log(config);
   return config;
