3b6b3a5531db0d0933c8c8eb8ccffe4d7bcf9b98
wibus-wee <1596355173@qq.com>
2022-12-02 08:07:32 +0800

    perf: use `master` handle to handle different return

--- apps/core/src/modules/comments/comments.controller.ts
+++ apps/core/src/modules/comments/comments.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { ApiOperation } from '@nestjs/swagger';
 import { ApiName } from '~/shared/common/decorator/openapi.decorator';
+import { IsMaster } from '~/shared/common/decorator/role.decorator';
 import { PagerDto } from '~/shared/dto/pager.dto';
 import { transformDataToPaginate } from '~/shared/transformers/paginate.transformer';
 import { CommentsBasicService } from './comments.basic.service';
@@ -12,12 +13,18 @@ export class CommentsController {
   constructor(private readonly commentsBasicService: CommentsBasicService) {}
 
   @Get('/')
-  // @Auth()
   @ApiOperation({ summary: '获取评论列表' })
-  async getRecentlyComments(@Query() query: PagerDto) {
+  async getRecentlyComments(
+    @Query() query: PagerDto,
+    @IsMaster() master: boolean,
+  ) {
     const { size = 10, page = 1, status = CommentStatus.Approved } = query;
     return transformDataToPaginate(
-      await this.commentsBasicService.getAllComments({ size, page, status }),
+      await this.commentsBasicService.getAllComments({
+        size,
+        page,
+        status: master ? status : CommentStatus.Approved,
+      }),
     );
   }
 
