5cb3175d0b4c645ecd4a97ccf3fdc7c65ea6d2b9
wibus-wee <1596355173@qq.com>
2022-12-04 16:11:31 +0800

    feat: basic service

--- apps/core/src/modules/friends/friends.basic.service.ts
+++ apps/core/src/modules/friends/friends.basic.service.ts
@@ -1,4 +1,46 @@
 import { Injectable } from '@nestjs/common';
+import { ReturnModelType } from '@typegoose/typegoose';
+import { InjectModel } from '~/shared/transformers/model.transformer';
+import { FriendsBasicModel, FriendStatus } from './friends.basic.model';
 
 @Injectable()
-export class FriendsBasicService {}
+export class FriendsBasicService {
+  constructor(
+    @InjectModel(FriendsBasicModel)
+    private readonly friendsBasicModel: ReturnModelType<
+      typeof FriendsBasicModel
+    >,
+  ) {}
+
+  get model() {
+    return this.friendsBasicModel;
+  }
+
+  async getAllFriends(isMaster: boolean) {
+    return await this.friendsBasicModel.find({
+      status: !isMaster && FriendStatus.Approved,
+    });
+  }
+
+  async createFriend(friend: FriendsBasicModel, isMaster: boolean) {
+    if (!isMaster) {
+      friend.status = FriendStatus.Pending;
+    }
+    return await this.friendsBasicModel.create(friend);
+  }
+
+  async updateFriend(id: string, friend: FriendsBasicModel, isMaster: boolean) {
+    if (!isMaster) {
+      friend.status = FriendStatus.Pending;
+    }
+    return await this.friendsBasicModel.findByIdAndUpdate(id, friend);
+  }
+
+  async updateFriendStatus(id: string, status: FriendStatus) {
+    return await this.friendsBasicModel.findByIdAndUpdate(id, { status });
+  }
+
+  async deleteFriend(id: string) {
+    return await this.friendsBasicModel.findByIdAndDelete(id);
+  }
+}
