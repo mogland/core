0c92b76e93f369e3a0b605f34910adbe78e48e44
wibus-wee <1596355173@qq.com>
2023-01-04 11:26:01 +0800

    feat: add comment reaction

--- apps/comments-service/src/comments-service.service.ts
+++ apps/comments-service/src/comments-service.service.ts
@@ -73,6 +73,21 @@ export class CommentsService {
     return this.CommentsModel.updateOne({ _id: id }, { $set: data });
   }
 
+  async addCommentReaction(id: string, reaction: CommentReactions) {
+    const comment = this.CommentsModel.findById(id);
+    if (!comment) {
+      throw new NotFoundException(ExceptionMessage.CommentNotFound);
+    }
+    return this.CommentsModel.updateOne(
+      { id },
+      {
+        $inc: {
+          [reaction]: 1,
+        },
+      },
+    );
+  }
+
   async deleteComment(id: string) {
     const comment = await this.CommentsModel.findOneAndDelete({ id });
     if (!comment) {
