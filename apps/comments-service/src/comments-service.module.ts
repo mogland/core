94e1c0123d00c4c5ed2a2efaf214f6d258638075
wibus-wee <1596355173@qq.com>
2023-01-04 11:44:39 +0800

    perf: remove useless authmodule

--- apps/comments-service/src/comments-service.module.ts
+++ apps/comments-service/src/comments-service.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from '~/libs/auth/src';
 import { DatabaseModule } from '~/libs/database/src';
-import { CommentsServiceController } from './comments-service.controller';
+import { CommentsController } from './comments-service.controller';
 import { CommentsService } from './comments-service.service';
 
 @Module({
-  imports: [DatabaseModule, AuthModule],
-  controllers: [CommentsServiceController],
+  imports: [DatabaseModule],
+  controllers: [CommentsController],
   providers: [CommentsService],
 })
 export class CommentsServiceModule {}
