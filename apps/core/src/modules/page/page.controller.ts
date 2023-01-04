9ae44710b57caa0c520fc74bdd6bcf978535a246
Wibus <62133302+wibus-wee@users.noreply.github.com>
2023-01-04 15:54:02 +0800

    perf(microservice): transport req to microservice (#559)

--- apps/core/src/modules/page/page.controller.ts
+++ apps/core/src/modules/page/page.controller.ts
@@ -12,7 +12,6 @@ import {
   Controller,
   Delete,
   Get,
-  HttpException,
   Inject,
   Param,
   Patch,
@@ -22,7 +21,6 @@ import {
 } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { ApiOperation } from '@nestjs/swagger';
-import { timeout, catchError, throwError } from 'rxjs';
 import { PageModel } from '~/apps/page-service/src/model/page.model';
 import { Auth } from '~/shared/common/decorator/auth.decorator';
 import { Paginator } from '~/shared/common/decorator/http.decorator';
@@ -31,6 +29,7 @@ import { PageEvents } from '~/shared/constants/event.constant';
 import { ServicesEnum } from '~/shared/constants/services.constant';
 import { MongoIdDto } from '~/shared/dto/id.dto';
 import { PagerDto } from '~/shared/dto/pager.dto';
+import { transportReqToMicroservice } from '~/shared/microservice.transporter';
 
 @Controller('page')
 @ApiName
@@ -41,73 +40,27 @@ export class PageController {
   @Paginator
   @ApiOperation({ summary: '获取页面列表' })
   async getPagesSummary(@Query() query: PagerDto) {
-    return this.page.send({ cmd: PageEvents.PageGetAll }, query).pipe(
-      timeout(1000),
-      catchError((err) => {
-        return throwError(
-          () =>
-            new HttpException(
-              err.message || '未知错误，请联系管理员',
-              err.status || 500,
-            ),
-        );
-      }),
-    );
+    return transportReqToMicroservice(this.page, PageEvents.PageGetAll, query);
   }
 
   @Get('/:id')
   @ApiOperation({ summary: '通过id获取页面详情' })
   @Auth()
-  async getPage(@Param() params) {
-    return this.page
-      .send({ cmd: PageEvents.PageGetByIdWithMaster }, params.id)
-      .pipe(
-        timeout(1000),
-        catchError((err) => {
-          return throwError(
-            () =>
-              new HttpException(
-                err.message || '未知错误，请联系管理员',
-                err.status || 500,
-              ),
-          );
-        }),
-      );
+  async getPage(@Param('id') id: string) {
+    return transportReqToMicroservice(this.page, PageEvents.PageGet, id);
   }
 
   @Get('/slug/:slug')
   @ApiOperation({ summary: '使用slug获取页面详情' })
   async getPageBySlug(@Param('slug') slug: string) {
-    return this.page.send({ cmd: PageEvents.PageGet }, slug).pipe(
-      timeout(1000),
-      catchError((err) => {
-        return throwError(
-          () =>
-            new HttpException(
-              err.message || '未知错误，请联系管理员',
-              err.status || 500,
-            ),
-        );
-      }),
-    );
+    return transportReqToMicroservice(this.page, PageEvents.PageGet, slug);
   }
 
   @Post('/create')
   @ApiOperation({ summary: '创建页面' })
   @Auth()
   async create(@Body() body: PageModel) {
-    return this.page.send({ cmd: PageEvents.PageCreate }, body).pipe(
-      timeout(1000),
-      catchError((err) => {
-        return throwError(
-          () =>
-            new HttpException(
-              err.message || '未知错误，请联系管理员',
-              err.status || 500,
-            ),
-        );
-      }),
-    );
+    return transportReqToMicroservice(this.page, PageEvents.PageCreate, body);
   }
 
   @Put('/:id')
@@ -115,37 +68,20 @@ export class PageController {
   @ApiOperation({ summary: '更新页面' })
   @Auth()
   async modify(@Body() body: PageModel, @Param() params: MongoIdDto) {
-    return this.page
-      .send({ cmd: PageEvents.PagePatch }, { id: params.id, body })
-      .pipe(
-        timeout(1000),
-        catchError((err) => {
-          return throwError(
-            () =>
-              new HttpException(
-                err.message || '未知错误，请联系管理员',
-                err.status || 500,
-              ),
-          );
-        }),
-      );
+    return transportReqToMicroservice(this.page, PageEvents.PagePatch, {
+      id: params.id,
+      body,
+    });
   }
 
   @Delete('/:id')
   @ApiOperation({ summary: '删除页面' })
   @Auth()
   async deletePage(@Param() params: MongoIdDto) {
-    return this.page.send({ cmd: PageEvents.PageDelete }, params.id).pipe(
-      timeout(1000),
-      catchError((err) => {
-        return throwError(
-          () =>
-            new HttpException(
-              err.message || '未知错误，请联系管理员',
-              err.status || 500,
-            ),
-        );
-      }),
+    return transportReqToMicroservice(
+      this.page,
+      PageEvents.PageDelete,
+      params.id,
     );
   }
 }
