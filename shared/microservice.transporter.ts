b2f2e1adaa188b5020d6774c4c2689aeb2dd43cf
wibus-wee <1596355173@qq.com>
2023-01-04 14:53:06 +0800

    feat: transport req to microservice

---
+++ shared/microservice.transporter.ts
@@ -0,0 +1,22 @@
+import { HttpException } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { timeout, catchError, throwError } from 'rxjs';
+
+export function transportReqToMicroservice(
+  client: ClientProxy,
+  cmd: string,
+  data: any,
+) {
+  return client.send({ cmd }, data).pipe(
+    timeout(1000),
+    catchError((err) => {
+      return throwError(
+        () =>
+          new HttpException(
+            err.message || '未知错误，请联系管理员',
+            err.status || 500,
+          ),
+      );
+    }),
+  );
+}
