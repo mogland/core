174cf307163f19da9b2bcc2480c3c61913f46e9d
wibus-wee <1596355173@qq.com>
2023-01-09 22:43:50 +0800

    feat: webhook support

--- apps/notification-service/src/notification-service.module.ts
+++ apps/notification-service/src/notification-service.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '~/libs/config/src';
+import { HelperModule } from '~/libs/helper/src';
 import { NotificationServiceController } from './notification-service.controller';
 import { NotificationService } from './notification-service.service';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule, HelperModule],
   controllers: [NotificationServiceController],
   providers: [NotificationService],
 })
--- apps/notification-service/src/notification-service.service.ts
+++ apps/notification-service/src/notification-service.service.ts
@@ -1,4 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '~/libs/config/src';
+import { HttpService } from '~/libs/helper/src/helper.http.service';
 
 @Injectable()
-export class NotificationService {}
+export class NotificationService {
+  constructor(
+    private readonly config: ConfigService,
+    private readonly http: HttpService,
+  ) {}
+
+  async getWebhookInEvent(event: string) {
+    const res: string[] = [];
+    const webhooks = await this.config.get('webhooks');
+    for (const webhook of webhooks) {
+      if (webhook.events.includes(event)) {
+        res.push(webhook.url);
+      }
+      if (webhook.events.includes('*')) {
+        res.push(webhook.url);
+      }
+    }
+    return res;
+  }
+
+  async sendWebhook(url: string, data: any) {
+    return await this.http.axiosRef.post(url, data);
+  }
+
+  async sendEvent(event: string, data: any) {
+    const webhooks = await this.getWebhookInEvent(event);
+    for (const webhook of webhooks) {
+      await this.sendWebhook(webhook, data);
+    }
+  }
+}
