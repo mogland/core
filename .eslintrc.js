eec260a39109c6d3b79f15f83fdcd00e0381a020
wibus-wee <1596355173@qq.com>
2023-01-21 23:30:46 +0800

    style(eslint): eslint rule improve [skip ci]

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
     'no-unused-vars': 'off', // or "@typescript-eslint/no-unused-vars": "off",
     '@typescript-eslint/no-unused-vars': 'off',
     'unused-imports/no-unused-imports': 'error',
+    '@typescript-eslint/no-non-null-assertion': 'off',
     '@typescript-eslint/consistent-type-imports': [
       'warn',
       {
