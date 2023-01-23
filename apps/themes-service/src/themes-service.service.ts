9fa733d10d0ff88204a30e59f57549a5d811f077
wibus-wee <1596355173@qq.com>
2023-01-23 18:52:27 +0800

    feat: validate theme

--- apps/themes-service/src/themes-service.service.ts
+++ apps/themes-service/src/themes-service.service.ts
@@ -1,19 +1,125 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { join } from 'path';
 import { ConfigService } from '~/libs/config/src';
 import { ThemeDto } from '~/libs/config/src/config.dto';
+import { THEME_DIR } from '~/shared/constants/path.constant';
 import { consola } from '~/shared/global/consola.global';
+import yaml from 'js-yaml';
+import {
+  ThemeConfig,
+  ThemeConfigItemAll,
+  ThemeConfigItemSelect,
+  ThemeConfigType,
+} from './theme.interface';
+import fs from 'fs';
 
 @Injectable()
 export class ThemesServiceService {
-  private readonly logger: Logger;
   private themes: ThemeDto[];
+  private dir: any[] = [];
   constructor(private readonly configService: ConfigService) {
-    this.logger = new Logger(ThemesServiceService.name);
-    this.configService.get('themes').then((themes) => {
-      this.themes = themes;
-      consola.success(
-        `主题服务已启动，共加载了${themes?.length || 0}个主题配置`,
-      );
-    });
+    this.configService
+      .get('themes')
+      .then((themes) => {
+        this.themes = themes;
+        consola.info(`共加载了${themes?.length || 0}个主题配置`);
+      })
+      .then(() => {
+        consola.info(`共检测到 ${fs.readdirSync(THEME_DIR).length} 个主题`);
+      })
+      .then(() => {
+        fs.readdirSync(THEME_DIR).forEach((theme) => {
+          this.validateTheme(theme, false);
+        });
+      })
+      .then(() => {
+        consola.success(
+          `合法主题检测完毕，共检测到 ${this.dir.length} 个合法主题`,
+        );
+      });
+  }
+
+  /**
+   * 验证主题合法性
+   */
+  validateTheme(theme: string, error = true) {
+    try {
+      if (!fs.existsSync(join(THEME_DIR, theme))) {
+        throw new InternalServerErrorException(`主题不存在`);
+      }
+      try {
+        fs.accessSync(path.join(THEME_DIR, theme), fs.constants.R_OK);
+      } catch (e) {
+        throw new InternalServerErrorException(`主题目录无读取权限`);
+      }
+      let config: ThemeConfig;
+      try {
+        const configString = fs.readFileSync(
+          join(THEME_DIR, theme, 'config.yaml'),
+          'utf-8',
+        );
+        config = yaml.load(configString) as ThemeConfig;
+      } catch (e) {
+        throw new InternalServerErrorException(
+          `主题配置文件读取失败, 请检查文件是否存在`,
+        );
+      }
+      if (!config?.configs) {
+        throw new InternalServerErrorException(
+          `主题配置文件格式错误, configs 字段不存在`,
+        );
+      }
+      config.configs.forEach((config) => {
+        if (!config?.name) {
+          throw new InternalServerErrorException(
+            `主题配置文件格式错误, "${config?.name}" 字段中的 name 字段不存在`,
+          );
+        }
+        if (!config?.type) {
+          throw new InternalServerErrorException(
+            `主题配置文件格式错误, "${
+              (config as ThemeConfigItemAll)?.name
+            }" 字段中的 type 字段不存在`,
+          );
+        }
+        if (!Object.values(ThemeConfigType).includes(config.type)) {
+          throw new InternalServerErrorException(
+            `主题配置文件格式错误, "${config?.name}" 字段中的 type 字段不合法`,
+          );
+        }
+        if (config.name.match(/[\u4e00-\u9fa5]/) && !config.key) {
+          throw new InternalServerErrorException(
+            `主题配置文件格式错误, "${config?.name}" 字段中的 key 字段不存在, 但是 name 字段是中文`,
+          );
+        }
+        if (
+          (config as ThemeConfigItemSelect).data &&
+          (config as ThemeConfigItemSelect).data.length
+        ) {
+          (config as ThemeConfigItemSelect).data.forEach((data) => {
+            if (data.name.match(/[\u4e00-\u9fa5]/) && !data.key) {
+              throw new InternalServerErrorException(
+                `主题配置文件格式错误, "${config?.name}" 字段中的 data 字段中的 "${data.name}" 字段中的 key 字段不存在, 但是 name 字段是中文`,
+              );
+            }
+            if (!data.value) {
+              throw new InternalServerErrorException(
+                `主题配置文件格式错误, "${config?.name}" 字段中的 data 字段中的 "${data.name}" 字段中的 value 字段不存在`,
+              );
+            }
+          });
+        }
+      });
+    } catch (e) {
+      if (error) {
+        throw new InternalServerErrorException(`[${theme}] ${e.message}`);
+      } else {
+        consola.info(`${chalk.red(`[${theme}] ${e.message}`)}`);
+        return false;
+      }
+    }
+
+    this.dir.push(theme);
+    return true;
   }
 }
