6e5750c9e585ac84d76607088b649060f6212688
Wibus <62133302+wibus-wee@users.noreply.github.com>
2023-01-25 09:54:22 +0800

    feat(helper): assets helper service (#620)

--- libs/helper/src/helper.module.ts
+++ libs/helper/src/helper.module.ts
@@ -1,9 +1,10 @@
 import { Provider, Global, Module } from '@nestjs/common';
 import { CacheModule } from '~/libs/cache/src';
+import { AssetsService } from './helper.assets.service';
 import { HttpService } from './helper.http.service';
 import { JWTService } from './helper.jwt.service';
 
-const providers: Provider<any>[] = [HttpService, JWTService];
+const providers: Provider<any>[] = [HttpService, JWTService, AssetsService];
 
 @Global()
 @Module({
