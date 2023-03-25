967aaf599e2ec0f30dcbfe6d4379adea3a01cca9
Wibus <62133302+wibus-wee@users.noreply.github.com>
2023-03-25 13:47:27 +0800

    perf: register only the necessary directories (#703)

--- shared/global/index.global.ts
+++ shared/global/index.global.ts
@@ -7,7 +7,6 @@ import 'zx-cjs/globals';
 import {
   DATA_DIR,
   LOG_DIR,
-  PUBLIC_DIR,
   THEME_DIR,
 } from '@shared/constants/path.constant';
 import { consola } from './consola.global';
@@ -20,23 +19,26 @@ function consoleMog() {
   console.log(MOG_NAME_ASCIIS_MAP['shadow']);
 }
 
-// 建立目录
-export function mkdirs() {
+// mkdirSync(PLUGIN_DIR, { recursive: true });
+// Logger.log(chalk.blue(`插件文件夹 已准备好: ${PLUGIN_DIR}`));
+
+// mkdirSync(PUBLIC_DIR, { recursive: true });
+// Logger.log(chalk.blue(`公共文件夹 已准备好: ${PUBLIC_DIR}`));
+
+export function mkBasedirs() {
   mkdirSync(DATA_DIR, { recursive: true });
   Logger.log(chalk.blue(`数据文件夹 已准备好: ${DATA_DIR}`));
+}
 
-  // mkdirSync(PLUGIN_DIR, { recursive: true });
-  // Logger.log(chalk.blue(`插件文件夹 已准备好: ${PLUGIN_DIR}`));
-
+export function mkThemeDir() {
+  mkBasedirs();
   mkdirSync(THEME_DIR, { recursive: true });
   mkdirSync(join(THEME_DIR), { recursive: true });
   Logger.log(chalk.blue(`主题文件夹 已准备好: ${THEME_DIR}`));
-
-  mkdirSync(PUBLIC_DIR, { recursive: true });
-  Logger.log(chalk.blue(`公共文件夹 已准备好: ${PUBLIC_DIR}`));
 }
 
 export function mkLogDir(service: string) {
+  mkBasedirs();
   mkdirSync(join(LOG_DIR, `${service}_service`), { recursive: true });
   Logger.log(chalk.blue(`日志文件夹 已准备好: ${LOG_DIR}`));
 }
@@ -57,6 +59,5 @@ export function registerGlobal() {
 export function register() {
   // registerGlobal();
   consoleMog();
-  mkdirs();
   // registerStdLogger();
 }
