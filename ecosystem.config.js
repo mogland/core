63c71b60d6f7db25421c7d2eb77b9b08e76963b1
wibus-wee <1596355173@qq.com>
2023-04-14 22:36:42 +0800

    perf(pm2): support pm2 config file [skip ci]

---
+++ ecosystem.config.js
@@ -0,0 +1,48 @@
+const { execSync } = require('child_process')
+const nodePath = execSync(`npm root --quiet -g`, { encoding: 'utf-8' }).split(
+  '\n',
+)[0]
+const args = {
+  autorestart: true,
+  watch: false,
+  max_memory_restart: '230M',
+  env: {
+    NODE_ENV: 'production',
+    NODE_PATH: nodePath,
+  },
+}
+
+module.exports = {
+  apps: [
+    {
+      name: 'Mog Gateway',
+      script: 'out/core/index.js',
+      ...args,
+    },
+    {
+      name: 'Mog Friends',
+      script: 'out/friends-service/index.js',
+      ...args,
+    },
+    {
+      name: 'Mog User',
+      script: 'out/user-service/index.js',
+      ...args,
+    },
+    {
+      name: 'Mog Pages',
+      script: 'out/page-service/index.js',
+      ...args,
+    },
+    {
+      name: 'Mog Notifications',
+      script: 'out/notification-service/index.js',
+      ...args,
+    },
+    {
+      name: 'Mog Comments',
+      script: 'out/comments-service/index.js',
+      ...args,
+    },
+  ],
+}
