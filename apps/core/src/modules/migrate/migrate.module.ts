f7344e8a063e213b532ea4bbcf81c1e9f9340d36
wibus-wee <1596355173@qq.com>
2023-04-30 12:17:17 +0800

    feat: inject module

--- apps/core/src/modules/migrate/migrate.module.ts
+++ apps/core/src/modules/migrate/migrate.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
 import { MigrateController } from './migrate.controller';
 import { MigrateService } from './migrate.service';
+import { ServicesEnum } from '~/shared/constants/services.constant';
+import { REDIS_TRANSPORTER } from '~/shared/constants/transporter.constants';
+import { ClientsModule } from '@nestjs/microservices';
+
+const services = {
+  [ServicesEnum.page]: ServicesEnum.page,
+  [ServicesEnum.user]: ServicesEnum.user,
+  [ServicesEnum.friends]: ServicesEnum.friends,
+  [ServicesEnum.comments]: ServicesEnum.comments,
+};
+
+const registers = Object.values(services).map((name) => ({
+  name,
+  ...REDIS_TRANSPORTER,
+}));
 
 @Module({
+  imports: [ClientsModule.register(registers)],
   controllers: [MigrateController],
   providers: [MigrateService]
 })
--- apps/core/src/modules/migrate/migrate.service.ts
+++ apps/core/src/modules/migrate/migrate.service.ts
@@ -1,4 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { ServicesEnum } from '~/shared/constants/services.constant';
 
 @Injectable()
-export class MigrateService {}
+export class MigrateService {
+  constructor(
+    @Inject(ServicesEnum.page) private readonly pageService: ClientProxy,
+    @Inject(ServicesEnum.user) private readonly userService: ClientProxy,
+    @Inject(ServicesEnum.friends) private readonly friendsService: ClientProxy,
+    @Inject(ServicesEnum.comments)
+    private readonly commentsService: ClientProxy,
+  ) {}
+}
