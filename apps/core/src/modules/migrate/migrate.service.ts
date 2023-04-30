d3c76a5f7f20d84790c63ed76885b44dd9f95494
wibus-wee <1596355173@qq.com>
2023-04-30 12:46:52 +0800

    feat: import user fn

--- apps/core/src/modules/migrate/migrate.service.ts
+++ apps/core/src/modules/migrate/migrate.service.ts
@@ -1,6 +1,17 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { ServicesEnum } from '~/shared/constants/services.constant';
+import {
+  MigrateCategory,
+  MigrateComment,
+  MigrateData,
+  MigrateFriend,
+  MigratePage,
+  MigratePost,
+  MigrateUser,
+} from './migrate.interface';
+import { transportReqToMicroservice } from '~/shared/microservice.transporter';
+import { UserEvents } from '~/shared/constants/event.constant';
 
 @Injectable()
 export class MigrateService {
@@ -12,5 +23,36 @@ export class MigrateService {
     private readonly commentsService: ClientProxy,
   ) {}
 
-  async import() {}
+  async importUser(data: MigrateUser) {
+    const exist = await transportReqToMicroservice(
+      this.userService,
+      UserEvents.UserGetMaster,
+      {},
+    ).catch((e) => {
+      if (e?.status == 404) {
+        return false;
+      }
+    });
+    // if not exist, register
+    if (!exist) {
+      return await transportReqToMicroservice(
+        this.userService,
+        UserEvents.UserRegister,
+        data,
+      );
+    } else {
+      return await transportReqToMicroservice(
+        this.userService,
+        UserEvents.UserPatch,
+        data,
+      )
+    }
+  }
+  async importComments(data: MigrateComment[]) {}
+  async importCategories(data: MigrateCategory[]) {}
+  async importPages(data: MigratePage[]) {}
+  async importFriends(data: MigrateFriend[]) {}
+  async importPosts(data: MigratePost[]) {}
+
+  async import(data: MigrateData) {}
 }
